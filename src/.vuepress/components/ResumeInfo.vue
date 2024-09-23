52a423f927e2a8e4d4827c7f49446208d89b327c
Violin Wang <violinwang@mail.ustc.edu.cn>
2024-09-23 19:17:17 +0800

    简历照片位置响应式布局

--- src/.vuepress/components/ResumeInfo.vue
+++ src/.vuepress/components/ResumeInfo.vue
@@ -32,7 +32,7 @@ function mergeTexts(texts: (string | number | undefined)[], delimiter = ' / ') {
             }
             result += text
         }
-    } 
+    }
     return result
 }
 const people = mergeTexts([name, gender, birthday])
@@ -45,24 +45,44 @@ const school: string = (() => {
 <template>
     <div class="info">
         <div class="text">
-            <Line :class-name="'people ' + gender" :text="people"/>
-            <Line class-name="school" :text="school"/>
-            <Line class-name="phone" :href="'tel:' + phone.replace(/-/g, '')" :text="phone"/>
-            <Line class-name="email" :href="'mailto:' + email" :text="email"/>
-            <Line class-name="github" :href="'https://'+github" :text="github"/>
-            <Line class-name="homepage" :href="homepage" :text="homepage"/>
-            <Line class-name="introduce" :text="introduce"/>
+            <Line :class-name="'people ' + gender" :text="people" />
+            <Line class-name="school" :text="school" />
+            <Line class-name="phone" :href="'tel:' + phone.replace(/-/g, '')" :text="phone" />
+            <Line class-name="email" :href="'mailto:' + email" :text="email" />
+            <Line class-name="github" :href="'https://' + github" :text="github" />
+            <Line class-name="homepage" :href="homepage" :text="homepage" />
+            <Line class-name="introduce" :text="introduce" />
         </div>
-        <ClientOnly>
+        <ClientOnly class="photo">
             <Suspense>
-                <Photo/>
+                <Photo />
             </Suspense>
         </ClientOnly>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use "@sass-palette/hope-config";
+
 .info {
     display: flex;
+}
+
+@media (min-width: hope-config.$tablet) {
+    .info {
+        align-items: center;
+        flex-direction: row;
+    }
+}
+
+@media (max-width: hope-config.$tablet) {
+    .info {
+        align-items: left;
+        flex-direction: column-reverse;
+    }
+
+    .photo {
+        margin: auto;
+    }
 }
 </style>
